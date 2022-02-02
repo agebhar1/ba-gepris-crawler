09f4f1704de31d0c3a047f7f5a3c57162c10872a
Andreas Gebhardt <agebhar1@googlemail.com>
2022-02-02 19:26:03 +0100

    [WIP] fix stage #1

--- src/main/scala/gepriscrawler/stage1/crawlresourcedetails/CrawledResourceDetailDataFromJsonAPIGraph.scala
+++ src/main/scala/gepriscrawler/stage1/crawlresourcedetails/CrawledResourceDetailDataFromJsonAPIGraph.scala
@@ -2,46 +2,42 @@ package gepriscrawler.stage1.crawlresourcedetails
 
 import akka.NotUsed
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-import akka.stream.scaladsl.{Flow, GraphDSL, Source}
+import akka.http.scaladsl.model.HttpRequest
+import akka.stream.scaladsl.{Flow, GraphDSL}
 import akka.stream.{FlowShape, Graph}
-import akka.util.ByteString
 
 import scala.concurrent.ExecutionContext
 
-object CrawledResourceDetailDataFromJsonAPIGraph
-{
+object CrawledResourceDetailDataFromJsonAPIGraph {
 
   def graph(resourceType: String)(implicit actorSystem: akka.actor.ActorSystem, streamMaterializer: akka.stream.Materializer, executionContext: ExecutionContext): Graph[FlowShape[String, (String, String)], NotUsed] = GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
 
     val resourceNameForUrlQuery = gepriscrawler.GeprisResources.resourceList(resourceType).resourceTyppeForUrlQuery
     val resourceIdToCrawl = b.add(Flow[String])
-    val resourceDetailJsonHttpRequest = b.add(Flow[String]
+
+    val resourceDetailJsonHttpRequest: FlowShape[String, (String, String)] = b.add(Flow[String]
       .mapAsync(2) { resourceId =>
         Http().singleRequest(
           HttpRequest(
             uri = s"https://gepris-extern.dfg.de/$resourceNameForUrlQuery/$resourceId"
           )
-        )
-          .map((resourceId -> _))
-      }
-    )
-
-    val crawledResourceDetailJSON: FlowShape[(String, HttpResponse), (String, String)] = b.add(Flow[(String, HttpResponse)]
-      .flatMapConcat { case (resourceId: String, resp: HttpResponse) =>
-        resp.status.intValue() match {
-          case 200 =>
-            resp.entity.dataBytes.fold[ByteString](ByteString(""))(_ ++ _)
-              .map(fullEntityByteString => (resourceId -> fullEntityByteString.utf8String))
-          case _ =>
-            println(s"Got non 200 status code for resource with id $resourceId; resp status: ${resp.status}; resp headers: ${resp.headers}")
-            throw new Exception("Got non-200 HTTP response from Gepris server")
+        ).flatMap { response =>
+          val (status, headers) = (response.status, response.headers)
+          status.intValue() match {
+            case 200 =>
+              response.entity.dataBytes
+                .runReduce(_ ++ _)
+                .map(body => resourceId -> body.utf8String)
+            case _ =>
+              println(s"Got non 200 status code for resource with id $resourceId; resp status: $status; resp headers: $headers")
+              throw new Exception("Got non-200 HTTP response from Gepris server")
+          }
         }
       }
     )
 
-    resourceIdToCrawl ~> resourceDetailJsonHttpRequest ~> crawledResourceDetailJSON
-    FlowShape(resourceIdToCrawl.in, crawledResourceDetailJSON.out)
+    resourceIdToCrawl ~> resourceDetailJsonHttpRequest
+    FlowShape(resourceIdToCrawl.in, resourceDetailJsonHttpRequest.out)
   }
 }
--- src/main/scala/gepriscrawler/stage1/crawlresourcedetails/CrawledResourceDetailDataGraph.scala
+++ src/main/scala/gepriscrawler/stage1/crawlresourcedetails/CrawledResourceDetailDataGraph.scala
@@ -1,50 +1,52 @@
 package gepriscrawler.stage1.crawlresourcedetails
 
-import gepriscrawler.CrawledResourceData
 import akka.NotUsed
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.headers.Cookie
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-import akka.stream.scaladsl.{Flow, GraphDSL, Source}
+import akka.stream.scaladsl.{Flow, GraphDSL}
 import akka.stream.{FlowShape, Graph}
-import akka.util.ByteString
+import gepriscrawler.CrawledResourceData
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 import scala.concurrent.ExecutionContext
 
-object CrawledResourceDetailDataGraph
-{
+object CrawledResourceDetailDataGraph {
 
   def graph(resourceType: String, crawledLanguage: String)(implicit actorSystem: akka.actor.ActorSystem, streamMaterializer: akka.stream.Materializer, executionContext: ExecutionContext): Graph[FlowShape[(Cookie, String), CrawledResourceData], NotUsed] = GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
 
     val resourceNameForUrlQuery = gepriscrawler.GeprisResources.resourceList(resourceType).resourceTyppeForUrlQuery
     val cookieAndUrlToCrawl = b.add(Flow[(Cookie, String)])
-    val resourceDetailPageHttpRequest = b.add(Flow[(Cookie, String)]
+
+    val resourceDetailPageHttpRequest: FlowShape[(Cookie, String), (String, Element)] = b.add(Flow[(Cookie, String)]
       .mapAsync(10) { case (cookie, resourceId) =>
         Http().singleRequest(
           HttpRequest(
-            uri = s"https://gepris.dfg.de/gepris/${resourceNameForUrlQuery}/$resourceId?language=$crawledLanguage",
+            uri = s"https://gepris.dfg.de/gepris/$resourceNameForUrlQuery/$resourceId?language=$crawledLanguage",
             headers = List(cookie)
           )
-        )
-          .map((resourceId, _))
+        ).flatMap {
+          response: HttpResponse =>
+            val (status, headers) = (response.status, response.headers)
+            status.intValue() match {
+              case 200 =>
+                print(s"\rReceived the detail page for the following resource for resource type '${resourceType}': id: $resourceId                                                   ")
+                response.entity.dataBytes
+                  .runReduce(_ ++ _)
+                  .map(body => (resourceId, Jsoup.parse(body.utf8String).body()))
+              case _ =>
+                println(s"Got non 200 status code for resource with id $resourceId; resp status: $status; resp headers: $headers")
+                throw new Exception("Got non-200 HTTP response from Gepris server")
+            }
+        }
       }
     )
 
-    val crawledResourceDetailData: FlowShape[(String, HttpResponse), CrawledResourceData] = b.add(Flow[(String, HttpResponse)]
-      .flatMapConcat { case (resourceId: String, resp: HttpResponse) =>
-        resp.status.intValue() match {
-          case 200 =>
-            print(s"\rReceived the detail page for the following resource for resource type '${resourceType}': id: $resourceId                                                   ")
-            resp.entity.dataBytes.fold[ByteString](ByteString(""))(_ ++ _)
-              .map(fullEntityByteString => (resourceId -> Jsoup.parse(fullEntityByteString.utf8String).body()))
-              .map(s => CrawledResourceData(resourceType, resourceId, crawledLanguage, s._2.toString))
-          case _ =>
-            println(s"Got non 200 status code for resource with id $resourceId; resp status: ${resp.status}; resp headers: ${resp.headers}")
-            throw new Exception("Got non-200 HTTP response from Gepris server")
-//            Source.empty
-        }
+    val crawledResourceDetailData: FlowShape[(String, Element), CrawledResourceData] = b.add(Flow[(String, Element)]
+      .mapConcat { case (resourceId: String, body: Element) =>
+        List(CrawledResourceData(resourceType, resourceId, crawledLanguage, body.toString))
       }
     )
 
